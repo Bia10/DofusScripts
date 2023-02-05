5edc37f3bcfbdafa11b78ebe3c0416f8b629ab99
Bia10 <t.locke1234@gmail.com>
2023-02-05 06:42:24 +0100

    wip

--- Modules/Spell.lua
+++ Modules/Spell.lua
@@ -3,8 +3,8 @@ local utils = require("Modules.Utils")
 local spell = {}
 
 spell.Data = {
-    { Id = 0,  NameFr = "Coup de poing", NameEn = "Punch" },
-    { Id = 13338,  NameFr = "Lancer de Pièces", NameEn = "Coin Throwing", ApCost = 2 },
+    { Id = 0,  NameFr = "Coup de poing", NameEn = "Punch", ApCost = 3, DefaultRange = 1 },
+    { Id = 13338,  NameFr = "Lancer de Pièces", NameEn = "Coin Throwing", ApCost = 2, DefaultRange = 8 },
  }
 
  spell.CastOnCellState = {
@@ -23,25 +23,68 @@ spell.Data = {
     end
  end
 
+ function spell.GetSpellParam(spellId, param)
+   for _, value in pairs(spell.Data) do
+        if (value.Id == spellId) then
+           if (param == "ApCost") then
+                return value.ApCost
+           elseif (param == "DefaultRange") then
+                return value.DefaultRange
+           end
+        end
+   end
+end
+
 function spell.CastOnCellStateToString(curCastOnCellState)
-        return utils.switch(spell.CastOnCellState, curCastOnCellState)
+    return utils.switch(spell.CastOnCellState, curCastOnCellState)
 end
 
 function spell.CanCastThisTurn(turnNumber)
-        -- Assuming that first turn its always castable, may not be
-      return fightAction:getCurrentTurn() == 1 or fightAction:getCurrentTurn() % turnNumber == 0
+     -- Assuming that first turn its always castable, may not be
+     return fightAction:getCurrentTurn() == 1 or fightAction:getCurrentTurn() % turnNumber == 0
 end
 
 function spell.HasApToCast(spellId, numberOfTimes)
-  for _, value in pairs(spell.Data) do
-     if (value.Id == spellId) then
-        if (numberOfTimes == 1) then
-           return fightCharacter:getAP() >= value.ApCost
-        elseif (numberOfTimes > 1) then
-           return (fightCharacter:getAP() * numberOfTimes) >= (value.ApCost * numberOfTimes)
-        end
-      end
+    local spellApCost = spell.GetApCost(spellId)
+    if (numberOfTimes == 1) then
+        return fightCharacter:getAP() >= spellApCost
+    elseif (numberOfTimes > 1) then
+        return (fightCharacter:getAP() * numberOfTimes) >= (spellApCost * numberOfTimes)
    end
+end
+
+function spell.IsTargetInRange(spellId, targetCellId)
+   local myCellId = fightCharacter:getCellId();
+   local distanceToTargetCell = fightAction:getDistance(myCellId, targetCellId)
+   local spellDefaultRange = spell.GetSpellParam(spellId, param)
+   local characterRangeBonus = fightCharacter:getRange()
+
+   -- TODO: we assume that spell range is modyfiable, we asume reuirement for line of sight
+   -- TODO: we assume a level 1 spell range as its actually progressive
+   -- TODO: we assume spell is not required to be cast in geometric shape (only in line, only in circle etc...)
+   if (distanceToTargetCell < (spellDefaultRange + characterRangeBonus)) then
+       return true
+   end
+end
+
+function spell.TryCastingAtCellId(spellId, myCellId, targetCellId)
+        -- Verification if we can cast spell on given cellId, result is a enum defined in spell.CastOnCellState
+        local spellCastOnCellState = fightAction:canCastSpellOnCell(myCellId, spellId, targetCellId)
+        -- Convert reason int value to string
+        local spellCastOnCellStateStr = spell.CastOnCellStateToString(spellCastOnCellState)
+
+        -- Its not possble to cast given spellId at given cellId for a reason
+        if (spellCastOnCellState ~= spell.CastOnCellState.CASTING_POSSIBLE) then
+                global:printError("Its not possible to cast: " ..spellId.. " on cellId: " ..targetCellId.. " reason: " ..spellCastOnCellStateStr)
+                ---------------------------------------------
+                -- Spell cast on cellId failure processing --
+                ---------------------------------------------
+                -- TODO: collect data, what is the reason of failure?
+                -- TODO: is possible to fix the reason of failure?
+        end
+
+        -- Casting possible then cast spell on cellId
+        fightAction:castSpellOnCell(spellId, targetCellId)
 end
 
 return spell
--- Templates/GenericAIScriptTemplate.lua
+++ Templates/GenericAIScriptTemplate.lua
@@ -14,9 +14,6 @@ function fightManagement()
             -- TODO: move away from enemy/ally? move closer to enemy/ally?
             -- TODO: move away from dranger (AOE spell, powerfull enemies, to gain range for long range spells)?   
 
-            -- Assuming out of range so move towards nearest enemy
-            fightAction:moveToWardCell(fightAction:getNearestEnemy())
-
             --------------------------------
             -- Action selection algorithm --
             --------------------------------
@@ -53,30 +50,26 @@ end
 function CastThrowingCoinsAtNearestEnemy(numberOfTimes)
     -- Get spellId of Coin Throwing
     local spellId = spell.GetIdByName("Coin Throwing", "En")
-    
+
     if (spell.HasApToCast(spellId, numberOfTimes)) then
         for i = 1, numberOfTimes do
             -- Get my cellId
             local myCellId = fightCharacter:getCellId();
             -- Get cellId of nearestEnemy
             local nearestEnemycellId = fightAction:getNearestEnemy()
-            -- Verification if we can cast spell on given cellId, result is a enum defined in spell.CastOnCellState
-            local spellCastOnCellState = fightAction:canCastSpellOnCell(myCellId, spellId, nearestEnemycellId)
-            -- Convert reason int value to string
-            local spellCastOnCellStateStr = spell.CastOnCellStateToString(spellCastOnCellState)
-
-            -- Its not possble to cast given spellId at given cellId for a reason
-            if (spellCastOnCellState ~= spell.CastOnCellState.CASTING_POSSIBLE) then
-                global:printError("Its not possible to cast: " ..spellId.. " on cellId: " ..myCellId.. " reason: " ..spellCastOnCellStateStr)
-                ---------------------------------------------
-                -- Spell cast on cellId failure processing --
-                ---------------------------------------------
-                -- TODO: collect data, what is the reason of failure?
-                -- TODO: is possible to fix the reason of failure?
+            -- Check range and try casting
+            if (spell.IsTargetInRange(spellId, nearestEnemycellId)) then
+                spell.TryCastingAtCellId(spellId, myCellId, nearestEnemycellId)
+            end
+            -- If i have mp left move towards target
+            if (fightCharacter:getMP() > 0) then
+                fightAction:moveToWardCell(nearestEnemycellId)
+                -- Again check range and try casting
+                if ((spell.IsTargetInRange(spellId, nearestEnemycellId))) then
+                    spell.TryCastingAtCellId(spellId, myCellId, nearestEnemycellId)
+                end
             end
 
-            -- Casting possible then cast spell on cellId
-            fightAction:castSpellOnCell(spellId, nearestEnemycellId)
         end
     end
 end
