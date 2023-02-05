dbef86d4ee7ae64db16aa6051e4b1d36847bb97e
Bia10 <t.locke1234@gmail.com>
2023-02-06 00:33:35 +0100

    wip

--- Modules/CharacterEquipment.lua
+++ Modules/CharacterEquipment.lua
@@ -5,6 +5,19 @@ characterEquipment.Slots = {
     { SlotIndex = 1, SlotNameEn = "Weapon" },
     { SlotIndex = 2, SlotNameEn = "Ring Left" },
     { SlotIndex = 3, SlotNameEn = "Belt" },
+    { SlotIndex = 4, SlotNameEn = "Ring Right" },
+    { SlotIndex = 5, SlotNameEn = "Boots" },
+    { SlotIndex = 6, SlotNameEn = "Hat" },
+    { SlotIndex = 7, SlotNameEn = "Cape" },
+    { SlotIndex = 8, SlotNameEn = "Pet" },
+    { SlotIndex = 9, SlotNameEn = "First Dofus Slot" },
+    { SlotIndex = 10, SlotNameEn = "Second Dofus Slot" },
+    { SlotIndex = 11, SlotNameEn = "Third Dofus Slot" },
+    { SlotIndex = 12, SlotNameEn = "Fouth Dofus Slot" },
+    { SlotIndex = 13, SlotNameEn = "Fifth Dofus Slot" },
+    { SlotIndex = 14, SlotNameEn = "Sixth Dofus Slot" },
+    { SlotIndex = 15, SlotNameEn = "Shield" },
+    { SlotIndex = 16, SlotNameEn = "Mount" }
 }
 
 function characterEquipment.SlotNameToIndex(slotName)
@@ -16,23 +29,35 @@ function characterEquipment.SlotNameToIndex(slotName)
 end
 
 function characterEquipment.HasItem(itemGid)
-    if inventory:itemCount(itemGid) > 0 then
-        return true
-    end
+    return inventory:itemCount(itemGid) > 0
+end
 
-    return false
+function characterEquipment.CanEquip(itemGid, equpmentSlotName)
+    local itemLevelRequirement = inventory:itemLevelId(itemGid)
+    local playerLevel = character:level()
+    --local isEquipmentSlotUsable = itemLevelRequirement.IsSlotUsable(equpmentSlotName)
+
+    return characterEquipment.HasItem(itemGid) and playerLevel >= itemLevelRequirement
 end
 
 function characterEquipment.EquipIntoSlot(itemGid, equpmentSlotName)
-    if characterEquipment.HasItem(itemGid) then
-        local equpmentSlotIndex = characterEquipment.SlotNameToIndex(equpmentSlotName)
 
-        if inventory:equipItem(itemGid, equpmentSlotIndex) == true then
-            global:printMessage("[CharacterEquipment] equiped item: " .. itemGid .. " into slot: " .. equpmentSlotName)
-            return
+    local equpmentSlotIndex = characterEquipment.SlotNameToIndex(equpmentSlotName)
+
+    if inventory:equipItem(itemGid, equpmentSlotIndex) == true then
+        global:printMessage("[CharacterEquipment] equiped item: " .. itemGid .. " into slot: " .. equpmentSlotName)
+        return
+    end
+
+    global:printMessage("[CharacterEquipment] failed to equip item: " ..
+        itemGid .. " into slot: " .. equpmentSlotName)
+end
+
+function characterEquipment.EquipMultipleItems(itemsDict)
+    for key, _ in pairs(itemsDict) do
+        if characterEquipment.CanEquip(itemsDict[key].ItemGid, itemsDict[key].SlotName) then
+            characterEquipment.EquipIntoSlot(itemsDict[key].ItemGid, itemsDict[key].SlotName)
         end
-
-        global:printMessage("[CharacterEquipment] failed to equip item: " .. itemGid .. " into slot: " .. equpmentSlotName)
     end
 end
 
