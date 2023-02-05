03faf5fe3615672c8d757693ad7c8146766af825
Bia10 <t.locke1234@gmail.com>
2023-02-05 16:19:24 +0100

    wip

--- Modules/Zapp.lua
+++ Modules/Zapp.lua
@@ -7,14 +7,34 @@ zapp.Data = {
     { ZaapId = 0, MapId = 120062979, MapPos = "1, -32", LocationNameFr = "Tainela", LocationNameEn = "Tainela" },
 }
 
-function zapp.DisoverZaapAtMap(mapId)
-    worldMove.ToMap(mapId)
 
+function zapp.TryMovingTo(mapId)
+    worldMove.ToMap(mapId)
     if fightCharacter:getCellId() == mapId then
-        if map:getNearestZaap(mapId) ~= 0 then
-            map:saveZaap()
-        end
+        global:printMessage("[Zaap] moved to zaap at mapId: " .. mapId .. ".")
+        return true
     end
+
+    global:printMessage("[Zaap] failed to move to zaap at mapId: " .. mapId .. ".")
+    return false
+end
+
+function zapp.TryDisoveringAtMap(mapId)
+    if zapp.TryMovingTo(mapId) == true then
+        local zaapId = map:getNearestZaap(mapId)
+
+        if zaapId ~= 0 then
+            global:printMessage("[Zaap] found zaap at mapId: " .. mapId .. " zappId: " .. zaapId .. " saving.")
+            map:saveZaap()
+            global:delay(math.random(1800, 4000))
+
+            return zaapId
+        end
+
+        global:printMessage("[Zaap] failed to locate zaap near mapId: " .. mapId .. ".")
+    end
+
+    return 0
 end
 
 return zapp
